ea43776d72c735d55222b895bd73f7079234f404
k-Sidney <kleubersidney@hotmail.com>
2024-05-05 15:50:36 -0300

    Projct Created

---
+++ backend/src/main/java/com/kscorp/ksmovie/KsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.kscorp.ksmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kscorp/ksmovie/KsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kscorp.ksmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
